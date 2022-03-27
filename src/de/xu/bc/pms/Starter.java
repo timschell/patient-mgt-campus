7cf8349606635b1d77f9eafa0ef5f05d5701d069
Mohammed AbuJarour <m.abujarour@xu-university.de>
2022-03-28 00:16:21 +0200

    Create a starting point for the program

---
+++ src/de/xu/bc/pms/Starter.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package de.xu.bc.pms;
+
+/**
+ * @author mohammed
+ *
+ */
+public class Starter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Let the system start ...");
+
+	}
+
+}
